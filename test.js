b0273914c6c7bbf96cc106313a4d0baea470ac13
JackDiSpade <alearci44@gmail.com>
2022-09-14 15:00:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,57 @@
+//firebase needs thses to figure out on how we r connecting to database
+//configuring the app on the front end to link with our firebase which we created on website
+var firebaseConfig = {
+  apiKey: "AIzaSyBa9UrS6dOUZry33QMnZfSKUFsDUBV6Zj0",
+  authDomain: "flappybird-1efe3.firebaseapp.com",
+  databaseURL: "https://flappybird-1efe3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "flappybird-1efe3",
+  storageBucket: "flappybird-1efe3.appspot.com",
+  messagingSenderId: "993302118393",
+  appId: "1:993302118393:web:f1de0e43d0ecd4d2f14728"
+};
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+        db = firebase.firestore(app);
+        firebase.firestore().settings({
+            cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED
+        });
+
+
+   const submitPlayers = document.querySelector("#submit_btn");
+   const name = document.querySelector("#name");
+   const leaderBoard = document.querySelector("#leaderBoard");
+   const hiscore = document.querySelector("#demo2");
+
+function renderCafe(doc){
+    let tr = document.createElement('tr');
+    let name = document.createElement('span');
+    let score = document.createElement('td');
+
+    tr.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    score.textContent = doc.data().score;
+
+    tr.appendChild(name);
+    tr.appendChild(score);
+
+    leaderBoard.appendChild(tr);
+}
+
+// real-time listener
+//snapshot an obj that represents your doc ..grab the data i t contains by calling data on it
+db.collection('players').orderBy('score', "desc").onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+        changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type == 'added'){
+            renderCafe(change.doc);}
+    })
+})
+
+
+
+
+
 var DEBUG = false;
 var SPEED = 180;
 var GRAVITY = 18;
@@ -168,6 +222,7 @@ function reset() {
     gameStarted = false;
     gameOver = false;
     score = 0;
+    hiscore = 0;
     credits.renderable = true;
     scoreText.setText("VOLA\nCOME\nUN\nGHIRO");
     instText.setText("TOCCA PER VOLARE\nCON L'UCCELLO");
